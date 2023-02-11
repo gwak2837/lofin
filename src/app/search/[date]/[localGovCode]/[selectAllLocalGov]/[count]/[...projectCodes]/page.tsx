92ca1c9339e8c1f5202fe3613590b07ef9304fd9
Taeuk Gwak <gwak2837@kakao.com>
2023-02-11 17:09:59 +0900

    Add center gov and SMART evaluation

--- src/app/search/[date]/[localGovCode]/[selectAllLocalGov]/[count]/[...projectCodes]/page.tsx
+++ src/app/search/[date]/[localGovCode]/[selectAllLocalGov]/[count]/[...projectCodes]/page.tsx
@@ -4,12 +4,12 @@ import { NEXT_PUBLIC_BACKEND_URL } from '../../../../../../../common/constants'
 import { PageProps } from '../../../../../../../common/types'
 import { formatPrice } from '../../../../../../../common/utils'
 
-type ExpenditureResponse = {
+type LocalGovResponse = {
   date: string
   expenditures: any[]
 }
 
-async function getExpenditures(params: Record<string, string & string[]>) {
+async function getLocalGov(params: Record<string, string & string[]>) {
   const { date, localGovCode, selectAllLocalGov, count, projectCodes } = params
 
   const searchParams = new URLSearchParams(`date=${date}`)
@@ -37,20 +37,55 @@ async function getExpenditures(params: Record<string, string & string[]>) {
   // if (!response.ok) throw new Error('Failed to fetch data') // `yarn build` not works
   if (!response.ok) return JSON.parse(await response.text()).message as string
 
-  return (await response.json()) as ExpenditureResponse | null
+  return (await response.json()) as LocalGovResponse | null
+}
+
+async function getCenterGov(params: Record<string, string & string[]>) {
+  const { date, localGovCode, selectAllLocalGov, count, projectCodes } = params
+
+  const searchParams = new URLSearchParams(`date=${date}`)
+
+  if (localGovCode !== 'null') {
+    searchParams.append('localGovCode', localGovCode)
+
+    if (selectAllLocalGov === 'true') {
+      searchParams.append('selectAllLocalGov', selectAllLocalGov)
+    }
+  }
+
+  if (count !== '20') {
+    searchParams.append('count', count)
+  }
+
+  if (projectCodes[0] !== '000') {
+    for (const projectCode of projectCodes) {
+      searchParams.append('projectCodes', projectCode)
+    }
+  }
+
+  const response = await fetch(`${NEXT_PUBLIC_BACKEND_URL}/expenditure?${searchParams}`)
+
+  // if (!response.ok) throw new Error('Failed to fetch data') // `yarn build` not works
+  if (!response.ok) return JSON.parse(await response.text()).message as string
+
+  return (await response.json()) as LocalGovResponse | null
 }
 
 export default async function SearchPage({ params }: PageProps) {
-  const response = await getExpenditures(params)
+  const localResponse = getLocalGov(params)
+  const centerResponse = getCenterGov(params)
+
+  const [localGov, centerGov] = await Promise.all([localResponse, centerResponse])
 
   return (
     <>
-      {response && typeof response === 'object' ? (
+      <h2 className="text-2xl m-6 text-center">SMART 평가</h2>
+
+      {(localGov || centerGov) && <h2 className="text-2xl m-6 text-center">결과</h2>}
+
+      {localGov && typeof localGov === 'object' ? (
         <>
-          <h2 className="text-2xl m-6 text-center">결과</h2>
-          <h3 className="text-xl m-4">
-            집행일자: {new Date(response.date).toISOString().slice(0, 10)}
-          </h3>
+          <h3 className="text-xl m-4">지방자치단체 재정</h3>
           <div className="overflow-x-auto h-screen">
             <table className="w-full my-2 whitespace-nowrap">
               <thead>
@@ -97,7 +132,7 @@ export default async function SearchPage({ params }: PageProps) {
                 </tr>
               </thead>
               <tbody>
-                {response.expenditures.map((expenditure, i) => (
+                {localGov.expenditures.map((expenditure, i) => (
                   <tr key={expenditure.id}>
                     <td className="p-2 text-center">{i + 1}</td>
                     <td className="p-2 text-center">{expenditure.sfrnd_name}</td>
@@ -119,7 +154,72 @@ export default async function SearchPage({ params }: PageProps) {
           </div>
         </>
       ) : (
-        <div>{response}</div>
+        <div>{localGov}</div>
+      )}
+
+      {centerGov && typeof centerGov === 'object' ? (
+        <>
+          <h3 className="text-xl m-4">중앙행정기관 재정</h3>
+          <div className="overflow-x-auto h-screen">
+            <table className="w-full my-2 whitespace-nowrap">
+              <thead>
+                <tr>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    순위
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    소관
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    회계
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    계정
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    분야
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    부문
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    프로그램
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    단위사업
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    세부사업
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    경비구분
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    지출구분
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    조회기준
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    전년도국회확정금액
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    전년도최종금액
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    정부안금액
+                  </th>
+                  <th className="p-4 top-0 sticky text-center bg-sky-200/90 backdrop-blur-sm font-semibold">
+                    국회확정금액
+                  </th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
+        </>
+      ) : (
+        <div>{centerGov}</div>
       )}
     </>
   )
